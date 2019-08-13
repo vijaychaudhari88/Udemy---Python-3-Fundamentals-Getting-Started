0c2292bd98365032876842da7a58b2a9c92deaf3
vijaychaudhari88 <vijaychaudhari117@gmail.com>
2019-08-13 19:57:33 +0530

    3.4 master

---
+++ 3.Python Getting Started/4. Python Comments.py
@@ -0,0 +1,12 @@
+print ("Hello World")
+
+def main():
+    print ("Hello1") #this is comment
+    # print ("Hello2") # this print command is commented, this not gone print
+    myPrint()
+
+def myPrint():
+    print ("Hello from myPrint")
+
+
+if __name__ == "__main__": main()
