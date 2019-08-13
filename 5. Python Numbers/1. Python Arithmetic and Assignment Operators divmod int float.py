dbd5f2bfdcfd7f011b08592040e8d20347c17402
vijaychaudhari88 <vijaychaudhari117@gmail.com>
2019-08-13 21:40:37 +0530

    5.1

---
+++ 5. Python Numbers/1. Python Arithmetic and Assignment Operators divmod int float.py
@@ -0,0 +1,35 @@
+
+a = 5
+print (a)
+
+a = 5.6
+print (a)
+
+a = 10
+print (a)
+a = 10 + 50
+print (a)
+a += 50
+print (a)
+a -= 30
+print (a)
+
+a = 89.0
+print ('a = ', a)
+b = 3
+print ('b = ', b)
+print ('a/b = ', a/b)
+print ('a//b', a//b)
+print ('a % b =', a%b)
+
+print ('divmod(a/b)', divmod(a,b))
+
+print ('int (5.66) ', int(5.66) )
+print (" int('11') ", int("11"))
+
+print (' float (5) ', float(5) )
+print (" float('22.66') ", float("22.66"))
+
+
+
+
