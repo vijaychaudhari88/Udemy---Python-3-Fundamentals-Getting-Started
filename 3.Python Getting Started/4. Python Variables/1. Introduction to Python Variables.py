7995d7524be7e3616b7e68ad46f33ede3c168ee1
vijaychaudhari88 <vijaychaudhari117@gmail.com>
2019-08-13 20:33:38 +0530

    4.1 master

---
+++ 3.Python Getting Started/4. Python Variables/1. Introduction to Python Variables.py
@@ -0,0 +1,18 @@
+
+varInt = 256
+print ("variable varInt value and type is ", varInt, type(varInt))
+varInt = 150
+print ("variable varInt value and type is ", varInt, type(varInt))
+varInt = "this string"
+print ("variable varInt value and type is ", varInt, type(varInt))
+varInt = 256.123456789
+print ("variable varInt value and type is ", varInt, type(varInt))
+
+varStr = "This is a string"
+print ("variable varStr value and type is ", varStr, type(varStr))
+
+varFloat = 25.7777777777
+print ("variable varFloat value and type is ", varFloat, type(varFloat))
+
+varA, varB = 15, 25
+print ("varA, varB ", varA, varB)
