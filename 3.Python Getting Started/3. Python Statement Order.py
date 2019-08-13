2280cc1ab5039649b60f1c3ec52b254b7af329f3
vijaychaudhari88 <vijaychaudhari117@gmail.com>
2019-08-13 18:24:28 +0530

    3.3 master

---
+++ 3.Python Getting Started/3. Python Statement Order.py
@@ -0,0 +1,23 @@
+print ("Hello World")
+
+def main():
+    print ("Hello1")
+    print ("Hello2")
+    myPrint()
+
+def func():
+    print ("Hello3")
+print ("Hello4")
+
+
+func()
+def myPrint():
+    print ("Hello from myPrint")
+if __name__ == "__main__": main()
+# func()
+
+# def myPrint():
+#     print ("Hello from myPrint")
+#   File "E:/Tuts-1/NEW/practise_code/Udemy - Python 3 Fundamentals Getting Started/3.Python Getting Started/3. Python Statement Order.py", line 6, in main
+#     myPrint()
+# NameError: global name 'myPrint' is not defined
