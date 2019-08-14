e64fe786ba020c3e437ccfb59822f0e2bbb9495c
vijaychaudhari88 <vijaychaudhari117@gmail.com>
2019-08-14 16:07:09 +0530

    6.1

---
+++ 6. Python Strings/1. String Literals In Python.py
@@ -0,0 +1,19 @@
+
+print ("this is a text in string literal")
+
+print ("'this is a text in single quote'")
+
+print ('"this is a text in double quoute"')
+
+print ("hello i'm text in python script")
+
+print ('character "say something something"')
+
+print ('''hello i'm a text in three quote, and i say "something something"''')
+
+print ('''hello i'm multiline strint 
+            i am a second line
+            i am third line
+            i am fourth line
+            i am last line
+''')
